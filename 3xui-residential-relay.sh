f24e0ebf751e85f31b77a23a171e36d50995a78d
505116666-coder <505116666@qq.com>
2026-09-23 18:43:43 +0800

    Update 3xui-residential-relay.sh

--- 3xui-residential-relay.sh
+++ 3xui-residential-relay.sh
@@ -935,7 +935,7 @@ def banner():
     color = sys.stdout.isatty() and os.environ.get('TERM') != 'dumb'
     colors = (33, 39, 45, 51, 45, 39, 33)
     say('')
-        if width >= 63:
+    if width >= 63:
         words = ('QIANYUDASHI',)
     elif width >= 35:
         words = ('QIANYU', 'DASHI')
